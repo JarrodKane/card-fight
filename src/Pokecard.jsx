699b3f00b842d3f8039ab9fc725627ea0e66bc8f
Trojan-Cat <kajarrod@gmail.com>
2020-10-01 18:10:18 +1000

    changed images

--- src/Pokecard.jsx
+++ src/Pokecard.jsx
@@ -3,12 +3,14 @@ import "./pokecard.css";
 
 function Pokecard({ pokemon }) {
   const { id, name, type, base_experience } = pokemon;
+  let pokeNumber = id.toString().padStart(3, '0');
+
   return (
     <div id={id} className="Pokecard">
       <h2 className="Pokecard-name">{name}</h2>
       <img
         className="Pokecard-img"
-        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
+        src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${pokeNumber}.png`}
         alt={name}
       />
       <p className="Pokecard-meta">Type: {type}</p>
