76eab043a5cc748fc5e0b663964623d1e94f4740
Trojan-Cat <kajarrod@gmail.com>
2020-10-03 16:08:34 +1000

    fixed up a class

--- src/Pokegame.jsx
+++ src/Pokegame.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Component } from 'react';
 import Pokedex from './Pokedex'
 
 
@@ -45,47 +46,57 @@ function countScore(deck) {
 
 
 
-function Pokegame(props) {
-    Pokedex.defaultProps = [
-        { id: 4, name: "Charmander", type: "fire", base_experience: 62 },
-        { id: 7, name: "Squirtle", type: "water", base_experience: 63 },
-        { id: 11, name: "Metapod", type: "bug", base_experience: 72 },
-        { id: 12, name: "Butterfree", type: "flying", base_experience: 178 },
-        { id: 25, name: "Pikachu", type: "electric", base_experience: 112 },
-        { id: 39, name: "Jigglypuff", type: "normal", base_experience: 95 },
-        { id: 94, name: "Gengar", type: "poison", base_experience: 225 },
-        { id: 133, name: "Eevee", type: "normal", base_experience: 65 },
-    ];
+class Pokegame extends Component {
+
+    static defaultProps = {
+        pokemon: [
+            { id: 4, name: 'Charmander', type: 'fire', base_experience: 62 },
+            { id: 7, name: 'Squirtle', type: 'water', base_experience: 63 },
+            { id: 11, name: 'Metapod', type: 'bug', base_experience: 72 },
+            { id: 12, name: 'Butterfree', type: 'flying', base_experience: 178 },
+            { id: 25, name: 'Pikachu', type: 'electric', base_experience: 112 },
+            { id: 39, name: 'Jigglypuff', type: 'normal', base_experience: 95 },
+            { id: 94, name: 'Gengar', type: 'poison', base_experience: 225 },
+            { id: 133, name: 'Eevee', type: 'normal', base_experience: 65 }
+        ]
+    };
 
 
-    const tim = getArry(Pokedex.defaultProps);
-    const eric = getArry(Pokedex.defaultProps);
 
 
 
-    const winner = () => {
+    render() {
+
+        const tim = getArry(this.props.pokemon);
+        const eric = getArry(this.props.pokemon);
+        let winner;
+
+
         const timScore = countScore(tim);
         const ericScore = countScore(eric);
+
         if (timScore > ericScore) {
-            return <div> Tim wins</div>
+            winner = <div> Tim wins</div>;
 
         } else {
-            return <div>Eric wins</div>
+            winner = <div>Eric wins</div>;
 
         }
 
+
+
+
+
+        return (
+            <div>
+                <Pokedex pokemons={tim} />
+                {winner}
+                <Pokedex pokemons={eric} />
+            </div>
+        )
     }
-
-
-
-    return (
-        <div>
-            <Pokedex pokemons={tim} />
-            {winner()}
-            <Pokedex pokemons={eric} />
-        </div>
-    )
-
 }
+
+
 
 export default Pokegame;
